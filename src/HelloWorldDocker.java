6b9cfe351c7dcee8bd4976ff3e3e7b9ccd9f5ee4
Vivek-open122 <vivekananda.m@bankopen.co>
2026-01-28 16:13:20 +0530

    Update HelloWorldDocker.java
    name changed and i

--- src/HelloWorldDocker.java
+++ src/HelloWorldDocker.java
@@ -1,7 +1,7 @@
-public class HelloWorld {
+public class HelloWorldDocker {
     public static void main(String[] args) {
         System.out.println("Hello World from Docker!");
-	for(int =0;i<5;i++)
+	for(int i=0;i<5;i++)
 {
 System.out.println("Hello World from Docker --"+i+1);
 }
